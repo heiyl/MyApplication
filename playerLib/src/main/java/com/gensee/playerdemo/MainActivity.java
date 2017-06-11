01081b4d9113caeddd3cdc1c4d87c68d3cba2278
heiyl <hyl_09@aliyun.com>
2017-06-11 09:51:26 +0800

    Initial commit

---
+++ playerLib/src/main/java/com/gensee/playerdemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.gensee.playerdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.gensee.player.activity.PlayerActivity;
+import com.gensee.voddemo.VodActivity;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_plmain);
+		
+		findViewById(R.id.live_btn).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, PlayerActivity.class));
+			}
+		});
+		
+		findViewById(R.id.vod_btn).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, VodActivity.class));
+			}
+		});
+	}
+}
